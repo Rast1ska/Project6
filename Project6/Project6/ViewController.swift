b2cb637bd5701897cf2f43bc91a28b3ec6f467b5
Salakhoff <salakhoff.ilfat@gmail.com>
2023-01-31 23:21:33 +0300

    Finish Result

--- Project6/Project6/ViewController.swift
+++ Project6/Project6/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 final class ViewController: UIViewController {
     
     // MARK: -Private Properties
+    private var shoppingList = [String]()
+    
+    // MARK: -Private Properties
     private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -53,45 +56,73 @@ final class ViewController: UIViewController {
     }
     
     @objc private func addNewProduct() {
-        
+        showAlertController(title: "Что вы хотите купить?", message: "Введите продукт, который вы хожите добавить в список")
     }
     
     @objc private func refreshResult() {
-        
+        shoppingList.removeAll(keepingCapacity: true)
+        tableView.reloadData()
     }
     
+    private func submit(_ product: String) {
+        if !product.isEmpty {
+            if !shoppingList.contains(product) {
+                shoppingList.insert(product, at: 0)
+                let indexPath = IndexPath(row: 0, section: 0)
+                tableView.insertRows(at: [indexPath], with: .automatic)
+                return
+            } else {
+                showAlertControllerError(title: "Ошибка!", message: "Такой продукт уже есть!")
+            }
+        } else {
+            showAlertControllerError(title: "Ошибка!", message: "Введите корректные данные!")
+        }
+    }
 }
 
 
 // MARK: - UITableViewDataSource
-extension UIViewController: UITableViewDataSource {
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        shoppingList.count
     }
     
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = String(indexPath.row)
+        cell.textLabel?.text = String(shoppingList[indexPath.row])
         return cell
     }
 }
 
 // MARK: -UIAlertController
-extension UIViewController {
-    private func showAlertController(title: String, message: String) {
+extension ViewController {
+    func showAlertController(title: String, message: String) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertController.addTextField { textField in
             textField.placeholder = "Write..."
         }
         
-        let actionOK = UIAlertAction(title: "Add", style: .default)
+        let actionOK = UIAlertAction(title: "Add", style: .default) { [weak self, weak alertController] action in
+            guard let product = alertController?.textFields?.first?.text else { return }
+            self?.submit(product)
+        }
         let actionCancel = UIAlertAction(title: "Cancel", style: .destructive)
         
         alertController.addAction(actionOK)
         alertController.addAction(actionCancel)
         present(alertController, animated: true)
     }
+    
+    func showAlertControllerError(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let actionOK = UIAlertAction(title: "OK", style: .default)
+        
+        alertController.addAction(actionOK)
+        present(alertController, animated: true)
+    }
 }
 
-
+// Нужно сделать анимацию добавления нового продукта
+// Поработать с  UIActivityViewController, чтобы можно было передать список продуктов
 
